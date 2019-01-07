30362c404db660eadc3ea0faa09fbc2701140673
Christian Nagel <christian@christiannagel.com>
2019-01-07 11:18:27 +0100

    first EF Core sample

---
+++ FirstSample/FirstSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using FirstSample.Models;
+using System;
+
+namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book { Title = "Professional C# 7", Publisher = "Wrox Press" };
+            var context = new BooksContext();
+            bool created = context.Database.EnsureCreated();
+            Console.WriteLine($"database created? {created}");
+            context.Books.Add(book);
+            int changed = context.SaveChanges();
+            Console.WriteLine($"records {changed} changed");
+        }
+    }
+}
