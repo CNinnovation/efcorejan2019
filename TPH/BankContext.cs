016cf99f8de70a6ca0e8da5e22cc77f667b173a8
Christian Nagel <christian@christiannagel.com>
2019-01-07 17:40:29 +0100

    multiple tables hierarchy

---
+++ TPH/BankContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TPHWithConventions
+{
+    public class BankContext : DbContext
+    {
+        private const string ConnectionString = @"server=(localdb)\MSSQLLocalDb;" +
+            "Database=LocalBankTest4;Trusted_Connection=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<CreditcardPayment>().HasKey(p => p.PaymentId);
+            //modelBuilder.Entity<CashPayment>().HasKey(p => p.PaymentId);
+        }
+
+        public DbSet<CreditcardPayment> CreditcardPayments { get; set; }
+        public DbSet<CashPayment> CashPayments { get; set; }
+    }
+}
---
+++ TPH/CreditcardPayment.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TPHWithConventions
+{
+    public class CreditcardPayment : Payment
+    {
+        public string CreditcardNumber { get; set; }
+    }
+}
